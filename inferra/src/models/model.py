b0fbd20ee8dcd44099c80ebaaee651a7525924e4
Mohamed-Ashraf273 <ma2736666@gmail.com>
2025-07-18 20:07:54 +0300

    remove comments

--- inferra/src/models/model.py
+++ inferra/src/models/model.py
@@ -1,26 +1,14 @@
 class Model:
     def __init__(self, data):
-        """
-        Initialize the model with data.
-        """
         self.data = data
 
     def fit(self):
-        """
-        Train the model on self.data.
-        """
         raise NotImplementedError("This method should be overridden by subclasses")
 
     def predict(self, input_data):
-        """
-        Predict output for input_data.
-        """
         raise NotImplementedError("This method should be overridden by subclasses")
 
     def evaluate(self, test_data):
-        """
-        Evaluate the model on test_data.
-        """
         raise NotImplementedError("This method should be overridden by subclasses")
 
     def export(self, filepath):
